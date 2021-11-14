78a891163e41c51f09f53c627788b5dd3982ca3f
Todd Demone <tdemone1@gmail.com>
2021-11-14 18:45:29 +0000

    move code inside a function; refactor by making variable names more descriptive; add JS docstring

--- timer1.js
+++ timer1.js
@@ -1,8 +1,18 @@
-const args = process.argv.slice(2);
-for (const arg of args) {
-  parseFloat(arg);
-  if (arg < 1 || isNaN(arg)) continue;
-  setTimeout((arg) => {
-    process.stdout.write('\x07');
-  }, arg * 1000);
-}
+/**
+ * Emits a beep after a specified amount of time has passed
+ * @param {Array} alarms An array of numbers, each number representing time in seconds 
+ */
+const timer1 = function(alarms) {
+  for (let alarm of alarms) {
+    alarm = parseFloat(alarm);
+    if (alarm < 1 || isNaN(alarm)) {
+      continue;
+    }
+    setTimeout((alarm) => {
+      process.stdout.write('\x07');
+    }, alarm * 1000);
+  }
+};
+const alarms = process.argv.slice(2);
+timer1(alarms);
+
