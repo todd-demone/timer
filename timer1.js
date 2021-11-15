9417867db75a47e615cd4831ccbc6911107aec33
Todd Demone <tdemone1@gmail.com>
2021-11-15 18:21:11 +0000

    refactor docstring

--- timer1.js
+++ timer1.js
@@ -1,7 +1,7 @@
 /**
  * An alarm clock which beeps after a specified amount of time has passed. 
  * An unlimited number of alarms can be specified in the `alarms` array.
- * @param {Array} alarms An array of numbers, each number representing time in seconds. 
+ * @param {Array} alarms An array of numbers (strings if inputted via command line arguments), each number representing time in seconds. 
  */
 const timer1 = function(alarms) {
   for (let alarm of alarms) {
